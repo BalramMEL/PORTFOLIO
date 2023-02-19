0136693b26bbf61cf57f48c9be483877713dea18
Balram Uddhav Melkunde <102988461+BalramMEL@users.noreply.github.com>
2023-02-19 13:39:39 +0530

    Completed header

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./App.css"
+import Header from './components/header/Header'
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    </>
+  )
+}
+
+export default App
+
