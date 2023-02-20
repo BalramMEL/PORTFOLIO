9cd6baf0275c6b630e024e4242f63fe1d4af7d0d
Balram Uddhav Melkunde <102988461+BalramMEL@users.noreply.github.com>
2023-02-20 16:51:17 +0530

    completed about and skills

---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Backend = () => {
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title">Backend developer</h3>
+
+      <div className="skills__box">
+        <div className="skills__group">
+          <div className="skills__data">
+            <i class="bx bxs-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Node.js</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+          <div className="skills__data">
+            <i class="bx bxs-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Express.js</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+          <div className="skills__data">
+            <i class="bx bxs-badge-check"></i>
+            <div>
+              <h3 className="skills__name">MongoDB</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="skills__group">
+          <div className="skills__data">
+            <i class="bx bxs-badge-check"></i>
+            <div>
+              <h3 className="skills__name">C++</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Backend;
