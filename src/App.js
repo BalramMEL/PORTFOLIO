ff87cb06c789eb373b3058b3170ef583c6cf9b19
Balram Uddhav Melkunde <102988461+BalramMEL@users.noreply.github.com>
2023-02-24 00:34:37 +0530

    footer, contact

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
-import About from "./components/about/About"
-import Skills from "./components/skills/Skills"
+import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
 import Services from "./components/services/Services";
 import Qualification from "./components/qualification/Qualification";
-
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import ScrollUp from "./components/scrollup/ScrollUp";
 
 const App = () => {
   return (
@@ -19,7 +21,10 @@ const App = () => {
         <Skills />
         <Services />
         <Qualification />
+        <Contact />
       </main>
+      <Footer />
+      <ScrollUp />
     </>
   );
 };
