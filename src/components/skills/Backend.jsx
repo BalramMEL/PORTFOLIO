97e1e19b19514759e91e9796168f449f10f45aa8
Balram Uddhav Melkunde <102988461+BalramMEL@users.noreply.github.com>
2023-03-30 22:50:54 +0530

    updated

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Backend = () => {
   return (
     <div className="skills__content">
-      <h3 className="skills__title">Backend developer</h3>
+      <h3 className="skills__title">Backend Skills</h3>
 
       <div className="skills__box">
         <div className="skills__group">
